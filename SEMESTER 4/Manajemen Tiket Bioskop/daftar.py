e907115fd34949e45f53cd7279dcff587398544d
Fajrul-Hidayat <60900123019@uin-alauddin.ac.id>
2025-07-04 02:11:47 +0800

    Update daftar.py

--- SEMESTER 4/Manajemen Tiket Bioskop/daftar.py
+++ SEMESTER 4/Manajemen Tiket Bioskop/daftar.py
@@ -2,5 +2,7 @@
 daftar_film = [
     {"id": 1, "judul": "Naruto", "genre": "Fiksi", "harga": 40000},
     {"id": 2, "judul": "Clannad", "genre": "Musikal", "harga": 38000},
-    {"id": 3, "judul": "Parasite", "genre": "Thriller", "harga": 39000}
+    {"id": 3, "judul": "Parasite", "genre": "Thriller", "harga": 39000},
+    {"id": 4, "judul": "One Piece", "genre": "Adventure", "harga": 48000},,
+    {"id": 5, "judul": "inuyasha", "genre": "Action", "harga": 32000}
 ]
