36b32300b223e7180b4dc7ebf984039f02383a67
HANAZIRA20 <60900123011@uin-alauddin.ac.id>
2025-07-04 00:55:51 +0800

    Buat-Daftar-Tiket

---
+++ SEMESTER 4/Manajemen Tiket Bioskop/daftar.py
@@ -0,0 +1,6 @@
+# daftar.py
+daftar_film = [
+    {"id": 1, "judul": "Naruto", "genre": "Fiksi", "harga": 40000},
+    {"id": 2, "judul": "Clannad", "genre": "Musikal", "harga": 38000},
+    {"id": 3, "judul": "Parasite", "genre": "Thriller", "harga": 39000}
+]
